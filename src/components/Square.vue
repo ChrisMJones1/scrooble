d72ec03af365cc1218b5402f90254d78ed0fc6c4
Christopher Jones <chrisjns679@gmail.com>
2020-06-23 10:25:40 -0400

    Basic board layout and structure init

---
+++ src/components/Square.vue
@@ -0,0 +1,18 @@
+<template >
+    <div v-if="index === 1" v-bind:style="{'background': 'aquamarine'}">{{letter}}</div>
+    <div v-else v-bind:style="{'background': 'pink'}">{{letter}}</div>
+</template>
+
+<script>
+    export default {
+        name: "Square",
+        props: ['letter', 'index']
+    }
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid black;
+
+    }
+</style>
